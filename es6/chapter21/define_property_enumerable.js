044d9d38f79814a9bfea16b23be3fa164f3b2a85
jueyini <jueyini@naver.com>
2017-09-13 21:24:51 +0900

    커밋

--- es6/chapter21/define_property_enumerable.js
+++ es6/chapter21/define_property_enumerable.js
@@ -3,7 +3,7 @@
 const arr = [3,1.5,9,2,5,2];
 arr.sum = function() { return this.reduce((a,x) => a+x ); }
 arr.avg = function() { return this.sum()/this.length; }
-//Object.defineProperty(arr,'sum',{enumerable:false});
-//Object.defineProperty(arr,'avg',{enumerable:false});
+Object.defineProperty(arr,'sum',{enumerable:false});
+Object.defineProperty(arr,'avg',{enumerable:false});
 
 for(var num in arr) console.log(num);
